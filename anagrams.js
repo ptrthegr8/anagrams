6f2f19cd7eb17bca56e41ebac0b1dd7a96b99fd0
Peter Marsh <p.marsh@mac.com>
2018-02-08 16:01:55 -0500

    update

--- anagrams.js
+++ anagrams.js
@@ -1,20 +1,12 @@
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
-var typedText = document.getElementById("input").value;
 document.getElementById("findButton").onclick = function () {
-    var typedText = document.getElementById("input").value;
-    for (let i = 0; i < words.length; i++) {
-        var word = words[i];
-        if (word === alphabetize(typedText)) {
-            console.log(word);
+        var typedText = document.getElementById("input").value;
+        for (let i = 0; i < words.length; i++) {
+            var word = words[i];
+            if (alphabetize(word) === alphabetize(typedText)) {
+               console.log(word);
+            }
         }
     }
-}
-//making all potential anagrams the same order of letters
-//should be able to be checked
-//if the same = anagram--if !== it is not an anagram
-
-
-//test
-//console.log(alphabetize("lair"))
