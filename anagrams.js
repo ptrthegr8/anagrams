88d4748c3b691bdca85c085ccc4d04684fc6c240
Peter Marsh <p.marsh@mac.com>
2018-02-08 17:00:10 -0500

    add'l changes

--- anagrams.js
+++ anagrams.js
@@ -1,12 +1,18 @@
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
+
 document.getElementById("findButton").onclick = function () {
         var typedText = document.getElementById("input").value;
         for (let i = 0; i < words.length; i++) {
+            var alphaTypedText = alphabetize(typedText);
             var word = words[i];
-            if (alphabetize(word) === alphabetize(typedText)) {
+            var alphaWord = alphabetize(word);
+            
+            if ( alphaWord === alphaTypedText) {
                console.log(word);
             }
         }
     }
+
+    
